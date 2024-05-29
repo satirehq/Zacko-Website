6446cbb384f7b710eb6a654b7cc3f7fdecf75549
Zackary Sanchez <satireheadquarters@gmail.com>
2024-05-28 21:28:59 -0700

    Added an .eleventy.js file

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+module.exports = function(eleventyConfig) {
+    //Getting css, js, img, etc.
+    eleventyConfig.addPassthroughCopy("src/art");
+    eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPassthroughCopy("src/js");
+    eleventyConfig.addPassthroughCopy("src/fonts");
+    eleventyConfig.addPassthroughCopy("src/media");
+    eleventyConfig.addPassthroughCopy("src/posts");
+    eleventyConfig.addCollection('posts', function(collectionApi) {
+      return collectionApi.getFilteredByGlob('src/posts/**/*.md');
+    })
+    
+    return {
+      dir: {
+        input: "src",
+        output: "published",
+        includes: "_includes",
+      }
+    }
+  };
