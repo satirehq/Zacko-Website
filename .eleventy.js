fa98752df8f0595630a0b31fb840160f2b8a18de
Zackary Sanchez <satireheadquarters@gmail.com>
2024-06-19 16:26:26 -0700

    Changed a few background colors on the homepage sidebars, along with a change in content to read

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,8 @@ module.exports = function(eleventyConfig) {
     })
     
     return {
+      markdownTemplateEngine: "njk",
+      htmlTemplateEngine: "njk",
       dir: {
         input: "src",
         output: "published",
